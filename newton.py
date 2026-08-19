26a1ac5bfc8c01884b0d0f3b9d0d7779e3df0a08
Cynthia302 <cynthia.s23@berkeley.edu>
2026-08-19 14:01:49 -0700

    docstring and formatting

--- newton.py
+++ newton.py
@@ -2,16 +2,17 @@ import pandas as pd
 import numpy as np
 import math
 
+
 def optimize(x, f):
     """
     x: starting value x0
     f: f(.) to optimize
     """
     e = 0.1
-    
+
     d1 = deriv(x, f)
     d2 = deriv2(x, f)
-    x1 = x - d1/d2
+    x1 = x - d1 / d2
 
     if abs(x1 - x) <= e:
         return x1
@@ -23,26 +24,27 @@ def deriv(x, f):
     """
     x: starting value x0
     f: f(.) to optimize
-    
+
     f(x+e) - f(x) / e -> f'(x)
     """
     e = 0.05
 
-    d = (f(x+e) - f(x))/e
-    
+    d = (f(x + e) - f(x)) / e
+
     return d
+
 
 def deriv2(x, f):
     """
     x: starting value x0
     f: f(.) to optimize
-    
+
     f'(x+e) - f'(x) / e -> f''(x)
     """
     e = 0.05
 
-    d = (deriv(x+e, f) - deriv(x, f))/e
-    
+    d = (deriv(x + e, f) - deriv(x, f)) / e
+
     return d
 
 
@@ -53,4 +55,3 @@ def quadratic(x):
     returns x^2 output
     """
     return x**2
-    
