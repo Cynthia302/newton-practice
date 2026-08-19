18c1c0464603cb3f8ea0690ccbd8ada4aa09b27f
Cynthia302 <cynthia.s23@berkeley.edu>
2026-08-19 16:24:25 -0700

    separate function and integer input

--- test_newton.py
+++ test_newton.py
@@ -6,8 +6,10 @@ import newton
 def test_basic_function():
     assert np.isclose(newton.optimize(2.95, np.cos), math.pi)
 
-def test_input_type():
+def test_input_function():
     with newton.optimize.raises(ValueError):
         newton.optimize(1, 1)
+        
+def test_input_startval():
     with newton.optimize.raises(ValueError):
         newton.optimize("string", 1)
